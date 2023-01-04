7a04caadfe410b93f07958a5053ff94dc944fd8c
TrainDoctor <traindoctor@protonmail.com>
2023-01-03 17:39:24 -0800

    Update entrypoint.sh, account for failures

--- builder/entrypoint.sh
+++ builder/entrypoint.sh
@@ -2,7 +2,16 @@
 
 cd /plugin
 
-pnpm i --frozen-lockfile
-pnpm run build
+if ! pnpm i --frozen-lockfile;
+then
+  printf "error code: %s\npnpm lockfile not found or other error occurred, please report this issue to the CI maintainer and the plugin developer." "$?"
+  exit 1
+fi
+
+if ! pnpm run build;
+then
+  printf "error code: %s\npnpm build failed, please report this issue to the CI maintainer and the plugin developer." "$?"
+  exit 1
+fi
 
 rsync -r --exclude "src/" --exclude "__pycache__" --exclude "node_modules" /plugin/ /out/
