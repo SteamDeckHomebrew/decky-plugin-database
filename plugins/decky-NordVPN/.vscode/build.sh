523c70308025ecf5359e2dbf4a64c2cfc3526b5d
werwolf2303 <66752411+werwolf2303@users.noreply.github.com>
2024-06-21 22:22:16 +0200

    Hopefully removed folder as told

--- plugins/decky-NordVPN/.vscode/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-echo "Building plugin in $(pwd)"
-printf "Please input sudo password to proceed.\n"
-
-# read -s sudopass
-
-# printf "\n"
-
-echo $sudopass | sudo $(pwd)/cli/decky plugin build $(pwd)
